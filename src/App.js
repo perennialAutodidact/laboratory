f56df6df3687953f7f35c989a5004ee17bb20285
unknown <keegood8@gmail.com>
2021-01-06 20:37:06 -0800

    setup canvas and add to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './Sass/App.scss';
-import Navbar from './components/layout/Navbar';
-import DotsAndLines from './components/experiments/dotsAndLines/DotsAndLines';
-
+import "./Sass/App.scss";
+import Navbar from "./components/layout/Navbar";
+import DotsAndLines from "./components/experiments/dotsAndLines/DotsAndLines";
+import SunClock from './components/experiments/sunClock/SunClock'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>  
-       {/* <DotsAndLines/> */}
+      <Navbar />
+      {/* <DotsAndLines/> */}
+      <SunClock bgColor={'#3a3a3a'}/>
+
     </div>
   );
 }
--- src/components/experiments/sunClock/SunClock.js
+++ src/components/experiments/sunClock/SunClock.js
@@ -1,15 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useRef, useEffect } from "react";
 import axios from 'axios'
 
-const SunClock = () => {
+const SunClock = ({ props }) => {
+  const canvasRef = useRef(null);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let animationFrameId;
 
+    canvas.width = window.innerWidth * .75;
+    canvas.height = window.innerHeight * .75;
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const render = () => {
+      context.fillStyle = '#3a3a3a';
+      context.fillRect(0, 0, canvas.width, canvas.height);
 
-export default SunClock
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () =>  window.cancelAnimationFrame(animationFrameId);
+
+  }, []);
+
+  return <canvas ref={canvasRef} {...props} />;
+};
+
+export default SunClock;
+
