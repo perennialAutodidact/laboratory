e5ae5d6e4a31406b59516ff78c2640e1da26f357
unknown <keegood8@gmail.com>
2021-01-07 00:04:14 -0800

    fetch sunrise/set times, style clock face, setup action types

--- src/components/experiments/sunClock/SunClock.js
+++ src/components/experiments/sunClock/SunClock.js
@@ -1,31 +1,61 @@
-import React, { useRef, useEffect } from "react";
-import axios from 'axios'
+import React, { useEffect } from "react";
+import axios from 'axios';
+import {setSunriseSunsetTimes} from '../../../state/actions/index'
 
-const SunClock = ({ props }) => {
-  const canvasRef = useRef(null);
+const SunClock = () => {
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    let animationFrameId;
+    const fetchSunriseSunsetTimes = async () => {
+      try {
+        let response = await axios.get('https://api.sunrise-sunset.org/json?lat=45.523064&lng=-122.676483')
 
-    canvas.width = window.innerWidth * .75;
-    canvas.height = window.innerHeight * .75;
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+      
+      
+    }
+    fetchSunriseSunsetTimes();
+  }, [])
 
-    const render = () => {
-      context.fillStyle = '#3a3a3a';
-      context.fillRect(0, 0, canvas.width, canvas.height);
+  return (
+    <div id="sunClock">
+      <div id="face">
 
-      animationFrameId = window.requestAnimationFrame(render);
-    };
-    render();
+      </div>
+    </div>
+  )
+}
 
-    return () =>  window.cancelAnimationFrame(animationFrameId);
 
-  }, []);
+export default SunClock
 
-  return <canvas ref={canvasRef} {...props} />;
-};
+// const SunClock = ({ props }) => {
+//   const canvasRef = useRef(null);
 
-export default SunClock;
+//   useEffect(() => {
+//     const canvas = canvasRef.current;
+//     const context = canvas.getContext("2d");
+//     let animationFrameId;
+
+//     canvas.width = window.innerWidth * .75;
+//     canvas.height = window.innerHeight * .75;
+
+//     const render = () => {
+//       context.fillStyle = '#3a3a3a';
+//       context.fillRect(0, 0, canvas.width, canvas.height);
+
+//       animationFrameId = window.requestAnimationFrame(render);
+//     };
+//     render();
+
+//     return () =>  window.cancelAnimationFrame(animationFrameId);
+
+//   }, []);
+
+//   return <canvas ref={canvasRef} {...props} />;
+// };
+
+// export default SunClock;
 
---
+++ src/state/actions/index.js
@@ -0,0 +1,6 @@
+import { SET_SUNRISE_SUNSET_TIMES } from '../constants/actionTypes'
+
+export const setSunriseSunsetTimes = (payload) => {
+  return {type: SET_SUNRISE_SUNSET_TIMES, payload}
+}
+
