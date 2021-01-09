da8147f312b81c8ee255c0c5d697346c49359394
unknown <keegood8@gmail.com>
2021-01-08 16:11:55 -0800

    switched back to canvas

--- src/components/experiments/sunClock/SunClock.js
+++ src/components/experiments/sunClock/SunClock.js
@@ -1,61 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import axios from 'axios';
+import {connect} from 'react-redux';
 import {setSunriseSunsetTimes} from '../../../state/actions/index'
 
-const SunClock = () => {
+const SunClock = ({dispatch, ...props}) => {
+  const fetchSunriseSunsetTimes = async () => {
+    try {
+      let response = await axios.get('https://api.sunrise-sunset.org/json?lat=45.523064&lng=-122.676483')
+
+      let {sunrise, sunset} = response.data.results;
+      console.log(sunrise, sunset);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const to24hr = (time) => {
+    console.log(time);
+  }
+
+  const canvasRef = useRef(null);
 
   useEffect(() => {
-    const fetchSunriseSunsetTimes = async () => {
-      try {
-        let response = await axios.get('https://api.sunrise-sunset.org/json?lat=45.523064&lng=-122.676483')
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let animationFrameId;
 
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-      
-      
-    }
     fetchSunriseSunsetTimes();
-  }, [])
 
-  return (
-    <div id="sunClock">
-      <div id="face">
+    canvas.width = window.innerWidth * .75;
+    canvas.height = window.innerHeight * .75;
 
-      </div>
-    </div>
-  )
+    const render = () => {
+      context.fillStyle = '#3a3a3a';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () =>  window.cancelAnimationFrame(animationFrameId);
+
+  }, []);
+
+  return <canvas ref={canvasRef} {...props} />;
+};
+
+const mapStateToProps = (state) => {
+  return {}
 }
 
-
-export default SunClock
-
-// const SunClock = ({ props }) => {
-//   const canvasRef = useRef(null);
-
-//   useEffect(() => {
-//     const canvas = canvasRef.current;
-//     const context = canvas.getContext("2d");
-//     let animationFrameId;
-
-//     canvas.width = window.innerWidth * .75;
-//     canvas.height = window.innerHeight * .75;
-
-//     const render = () => {
-//       context.fillStyle = '#3a3a3a';
-//       context.fillRect(0, 0, canvas.width, canvas.height);
-
-//       animationFrameId = window.requestAnimationFrame(render);
-//     };
-//     render();
-
-//     return () =>  window.cancelAnimationFrame(animationFrameId);
-
-//   }, []);
-
-//   return <canvas ref={canvasRef} {...props} />;
-// };
-
-// export default SunClock;
+export default connect(mapStateToProps)(SunClock);
 
