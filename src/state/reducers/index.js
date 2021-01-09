ef37192c0eeda0faf23a89798ce41b2536204b65
unknown <keegood8@gmail.com>
2021-01-08 16:07:45 -0800

    added initialState

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,7 +1,17 @@
-const rootReducer = (state, action) = () => {
+import {SET_SUNRISE_SUNSET_TIMES} from '../constants/actionTypes';
+import {initialState} from '../'
+
+const rootReducer = (state=initialState, action) => {
   switch(action.type){
     default:
       return state
+    
+    case SET_SUNRISE_SUNSET_TIMES:
+      return {
+        ...state,
+        sunrise: action.payload.sunrise,
+        sunset: action.payload.sunset
+      }
   }
 }
 
