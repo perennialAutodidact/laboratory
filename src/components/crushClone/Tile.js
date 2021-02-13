e840d4fbd2e2be473b4b6052c7fc1d68d2f7be4b
unknown <keegood8@gmail.com>
2021-02-12 22:50:38 -0800

    added crushclone

---
+++ src/components/crushClone/Tile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tile = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tile
--- src/state/slices/crushCloneSlice.js
+++ src/state/slices/crushCloneSlice.js
@@ -1,13 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  score: 0
+}
+
 const crushCloneSlice = createSlice({
   name: "crushClone",
-  initialState: {
-    score: 0,
-  },
+  initialState: initialState,
   reducers: {
-    resetScore(state, action){},
-    increaseScore(state, action) {},
+    resetScore(state, action){
+      return {
+        score: 0
+      }
+    },
+    increaseScore(state, action) {
+      return {
+        score: state.store + 1
+      }
+    },
     decreaseScore(state, action) {}
   },
 });
