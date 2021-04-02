53a2548ac7866c095163c027d9fe01cecf782a99
unknown <keegood8@gmail.com>
2021-04-01 17:57:17 -0700

    fixed trie.addWords()

--- src/components/experiments/sunClock/AutoCompleteSelect/AutoCompleteSelect.js
+++ src/components/experiments/sunClock/AutoCompleteSelect/AutoCompleteSelect.js
@@ -2,47 +2,50 @@ import React, { useState, useEffect, useRef } from "react";
 import { Trie } from "./autoComplete";
 import titleize from "../../../../utilities/titleize";
 import useKeyPress from "../../../../utilities/useKeyPress";
+import useVisible from "../../../../utilities/useVisible";
 import { CgKeyhole } from "react-icons/cg";
 
-const Option = ({ label, id, selected, first, last }) => {
+const Option = ({ text, id, selected, first, last }) => {
   let optionRef = useRef(null);
 
   return (
     <div
       className={
         "auto-complete-option " +
-        (selected ?    "selected " : "") +
-        (   first ? "first-option" : "") +
-        (    last ?  "last-option" : "")
+        (selected ? "selected " : "") +
+        (first ? "first-option" : "") +
+        (last ? "last-option" : "")
       }
       ref={(el) => (optionRef = el)}
     >
-      <span id={id}>
-        {titleize(label)}
-      </span>
+      <span id={id}>{titleize(text)}</span>
     </div>
   );
 };
+
+////////////////////////////////////////////////////////////////////////////////
 
 /**
  * @param {function} formDataSetter - the function to update the select's value in the parent form
  * @param {Object[]} allOptions - list of options for autocomplete
  * @param {string} fieldName - name attribute of the select field
  * @param {number} optionsShown - number of select options to display
- *
+ * 
  */
 // formDataSetter - the function to update the select's value in the parent form
 // allOptions - list of options for autocomplete
 // fieldName - name of the input field
-const AutoCompleteSelect = ({allOptions, fieldName, formDataSetter, optionsShown}) => {
-  const [state, setState] = useState({
-    firstOption: 0,
-    lastOption: 5,
-    selectedValue: null,
-  });
-
+const AutoCompleteSelect = ({
+  allOptions,
+  fieldName,
+  formDataSetter,
+  optionsShown,
+}) => {
+  const [rangeStart, setRangeStart] = useState(0);
+  const [rangeEnd, setRangeEnd] = useState(5);
   const [query, setQuery] = useState("");
-  const [selectedOption, setSelectedOption] = useState(3);
+  const [selectedOption, setSelectedOption] = useState(0);
+  const [selectedValue, setSelectedValue] = useState(null);
   const [trie] = useState(new Trie());
   const [results, setResults] = useState([]);
 
@@ -50,6 +53,9 @@ const AutoCompleteSelect = ({allOptions, fieldName, formDataSetter, optionsShown
   const upKeyPress = useKeyPress("ArrowUp");
   const downKeyPress = useKeyPress("ArrowDown");
   const enterKeyPress = useKeyPress("Enter");
+
+  // set up listener to detent click outside of this element
+  const {allOptionsRef, isVisible, setIsVisible} = useVisible(false);
 
   // change 'query' in state when input value changes
   const onChange = (e) => {
@@ -59,30 +65,30 @@ const AutoCompleteSelect = ({allOptions, fieldName, formDataSetter, optionsShown
 
   // update which select options are shown based on the currently selected option
   const updateOptions = () => {
-
-  }
+    console.log(rangeStart, selectedOption, rangeEnd);
+  };
 
   // Add all lowercase state names to Trie
   useEffect(() => {
     if (trie) {
       // add individual words to trie rather than all at once
       // in order to add each as a lowercase word, rather than the whole object
-      props.allOptions.forEach((option) => {
-        trie.addWords(option.label.toLowerCase());
-      });
+        trie.addWords(allOptions);
     }
-  }, [props.allOptions, trie]);
+  }, [allOptions, trie]);
 
   // when the query changes, find all words in the Trie that begin with the query string
   useEffect(() => {
     let newResults;
     if (query === "") {
       newResults = [];
+      setIsVisible(false);
     } else {
       newResults = trie.find(query.toLowerCase());
+      setIsVisible(true);
     }
-    formDataSetter(query);
-    setResults(newResults);
+    formDataSetter(query); // update form state
+    setResults(newResults); // update auto complete results
   }, [query, formDataSetter, trie]);
 
   // change the selected auto complete option using arrow keys
@@ -92,20 +98,15 @@ const AutoCompleteSelect = ({allOptions, fieldName, formDataSetter, optionsShown
         setSelectedOption(selectedOption - 1);
       }
     } else if (downKeyPress) {
-      if (
-        selectedOption < props.optionsShown - 1 &&
-        selectedOption < results.length - 1
-      ) {
+      if (selectedOption < results.length - 1) {
         setSelectedOption(selectedOption + 1);
-      } else if(selectedOption === state.firstOption + props.optionsShown){
-        alert('lala')
       }
     } else if (enterKeyPress) {
       console.log("enter pressed");
     }
   }, [
     selectedOption,
-    props.optionsShown,
+    optionsShown,
     results,
     upKeyPress,
     downKeyPress,
@@ -114,25 +115,20 @@ const AutoCompleteSelect = ({allOptions, fieldName, formDataSetter, optionsShown
 
   return (
     <div id="auto-complete-select">
-      <input
-        type="text"
-        name={props.fieldName}
-        value={query}
-        onChange={onChange}
-      />
+      <input type="text" name={fieldName} value={query} onChange={onChange} />
 
-      {results && results.length > 0 ? (
+      {isVisible && results && results.length > 0 ? (
         <div className="auto-complete-options">
           {/* display the first 6 options */}
-          {results.map((label, i) =>
+          {results.map((text, i) =>
             i < 100 ? (
               <Option
-                label={label}
+                text={text}
                 id={i}
                 key={i}
                 selected={selectedOption === i}
-                first={state.firstOption === i}
-                last={state.lastOption === i}
+                first={rangeStart === i}
+                last={rangeEnd === i}
               />
             ) : (
               ""
--- src/components/experiments/sunClock/AutoCompleteSelect/autoComplete.js
+++ src/components/experiments/sunClock/AutoCompleteSelect/autoComplete.js
@@ -1,5 +1,10 @@
-const stateList = require('../../../constants/stateList');
-
+/**
+ * @param {string} char - character held within the node. default: null
+ *
+ * let node = new TrieNode('a')
+ *
+ * console.log(node.char) -> 'a'
+ */
 export class TrieNode {
   constructor(char = null) {
     this.char = char;
@@ -8,105 +13,127 @@ export class TrieNode {
     this.wordComplete = false;
   }
 
-  getWord(){
+  /**
+   *
+   * @returns the full word of which this node is part
+   */
+  getWord() {
     let node = this;
     let letters = [];
 
-    while(node){
-      letters.unshift(node.char)
+    while (node) {
+      letters.unshift(node.char);
       node = node.parent;
     }
 
-    return letters.join('');
+    return letters.join("");
   }
 }
 
+/**
+ * A collection of words stored in a series of TrieNodes
+ *
+ * @param {array} [words="[]"] - Array of strings to be added to the Trie. Each will be converted to lowercase before adding
+ * 
+ */
 export class Trie {
   constructor(words = []) {
     this.root = new TrieNode(); // first node has null value
-
+    this.addWords(words);
   }
 
-  addWords(...words) {
+  /**
+   * 
+   * @param {array} words - Array of strings to be added to the Trie. Each will be converted to lowercase before adding
+   */
+  addWords(words) {
     let node = this.root;
 
-    words.forEach(word => {
-
-      [...word].forEach((letter, i) => {
-        if (letter in node.children) {
+    words.forEach((word) => {
+      // lowercase each word and add it to the trie
+      [...word.toLowerCase()].forEach((letter, i) => {
+        if (node.children.hasOwnProperty(letter)) {
+          // if the letter is already in the trie,
+          // that becomes the current node
           node = node.children[letter];
         } else {
+          // if the letter isn't in the trie,
+          // add the letter to the trie and move to the new letter
           node.children[letter] = new TrieNode(letter);
           node.children[letter].parent = node;
           node = node.children[letter];
         }
-        
-        if(i === word.length-1){
+
+        // at the last letter, the word is complete,
+        if (i === word.length - 1) {
           node.wordComplete = true;
+          node = this.root; // go back to root node
         }
-        
-      })
+      });
     });
   }
 
+  /**
+   * Recursive function to find all the words that start from a particular node
+   * 
+   * @param {TrieNode} node starting node
+   * @param {array} words running list of words
+   * @returns array of words starting at the given node
+   */
   findAllWords = (node, words) => {
-
-    if(node.wordComplete){
-      words.push(node.getWord());  
+    if (node.wordComplete) {
+      words.push(node.getWord());
     }
-    
+
     for (let child in node.children) {
-      this.findAllWords(node.children[child], words)
+      this.findAllWords(node.children[child], words);
     }
 
-    return words
-  }
+    return words;
+  };
 
+  /**
+   * 
+   * @returns array of all the words currently in the Trie
+   */
   allWords() {
     let node = this.root;
     let words = [];
 
-    this.findAllWords(node, words)
-  
-    return words
+    this.findAllWords(node, words);
+
+    return words;
   }
 
-  find(prefix){
+  /**
+   * Recursive function to find all the words in the Trie 
+   * which begin with the given prefix
+   * @param {string} prefix 
+   * @returns array of words beginning with the given prefix
+   */
+  find(prefix) {
     let node = this.root;
-    let words = []
+    let words = [];
 
-    for(let i=0; i<prefix.length; i++){
-      if(node.children[prefix[i]]){
-        node = node.children[prefix[i]]
+    for (let i = 0; i < prefix.length; i++) {
+      if (node.children[prefix[i]]) {
+        node = node.children[prefix[i]];
       } else {
-        return words
+        return words;
       }
     }
-      this.findAllWords(node, words)
+    this.findAllWords(node, words);
 
-      return words;
-    
+    return words;
   }
 }
 
+let wordList = ['cat', 'catch', 'act', 'ale']
 
-// let t = new Trie();
+let t = new Trie();
 
-// // t.addWord("elephant");
-// // t.addWord("ear");
-// // t.addWord("carrot")
-// // t.addWord("carpool")
-// // t.addWord("cathedral")
-// // t.addWord("clavical")
+t.addWords(wordList);
 
 
-// stateList.forEach(state=>{
-//   t.addWords(state.label.toLowerCase())
-// })
-
-// let words = t.allWords();
-// console.log(words);
-
-
-
-
+let words = t.allWords();
+console.log(words);
