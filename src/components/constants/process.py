361cf8ececd2ad8b10290223e3ffa066ead1061e
keegangood <keegood8@gmail.com>
2021-04-05 18:26:37 -0700

    organize dictionary with country abbreviations as keys and name/list of cities as values

--- src/components/constants/process.py
+++ src/components/constants/process.py
@@ -9,17 +9,15 @@ delete_keys = ['languages', 'currency','capital', 'phone', 'native','continent']
 for country in countries:
     for key in delete_keys:
         del countries[country][key]
-    other_chars = 'АĀÁÀÅÄÆÇČĆСḎÐĐŒÈÉĒƏḨНḤĪȘЗİÍĽПЈĶКĦŁÑБИŌÖÓОŐÎÒØЧЦÞРГŘṞŞŚŠŢȚṬŪÜÚŬÝŻŽẔ'
+    other_chars = 'ÀÁÂÄÅÆÇÈÉÍÎÐÑÒÓÔÖØÚÜÝÞĀĆČĐĒĦĪİĶĽŁŌŐŒŘŚŞŠŢŪŬŻŽƏȘȚЈАБГЗИКНОПРСЦЧḎḐḤḨṞṬẔẤ'
     countries[country]['cities'] = {letter:[] for letter in ABCs + other_chars}
 
 
 with open('newCountryList.json', 'r', encoding='utf-8') as file:
     cities = json.loads(file.read())
 
-# print(countries)
-# print(cities[:3])
 
-for city in cities[len(cities)/2:]:
+for city in cities[len(cities)//2:]:
     print(city['name'])
     countries[city['country']]['cities'][city['name'][0].upper()].append(city['name'])
 
