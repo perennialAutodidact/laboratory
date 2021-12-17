b99438ecbda5f16c3c5f709102310cc0685838b7
keegangood <keegood8@gmail.com>
2021-12-17 15:17:27 -0800

    show search results

--- frontend/src/components/experiments/sunClock/Form.js
+++ frontend/src/components/experiments/sunClock/Form.js
@@ -1,148 +1,161 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { GrClose } from 'react-icons/gr'
-import { TimelineLite, TweenMax, Elastic } from 'gsap' // gsap animation library
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState, useRef, useEffect } from "react";
+import { GrClose } from "react-icons/gr";
+import { TimelineLite, TweenMax, Elastic } from "gsap"; // gsap animation library
+import { useDispatch, useSelector } from "react-redux";
 import {
   toggleForm,
   fetchCoords,
   fetchSunData,
   setSunData,
   setCoords,
-  setLocation
-} from '../../../state/slices/sunClockSlice' // pull in actions from slice
-import { countryList } from '../../constants/countryList'
-import { stateList } from '../../constants/stateList'
+  setLocation,
+} from "../../../state/slices/sunClockSlice"; // pull in actions from slice
+import { countryList } from "../../constants/countryList";
+import { stateList } from "../../constants/stateList";
 // import { cityList } from '../../constants/cityList'
 
-import AutoCompleteSelect from './AutoCompleteSelect/AutoCompleteSelect'
-import Alert from './Alert'
+import AutoCompleteSelect from "./AutoCompleteSelect/AutoCompleteSelect";
+import Alert from "./Alert";
+import SearchResults from "./SearchResults";
 
 const Form = () => {
-  const dispatch = useDispatch()
-  const { showForm } = useSelector(state => state.sunClock)
+  const dispatch = useDispatch();
+  const { showForm, coordsSearchResults } = useSelector(
+    (state) => state.sunClock
+  );
 
   const [formData, setFormData] = useState({
-    city: '',
-    lat: '',
-    lng: ''
-  })
-  const { city, lat, lng } = formData
-  const [stateName, setStateName] = useState('')
-  const [country, setCountry] = useState('')
-  const [showCityField, setShowCityField] = useState(false)
-  const [showStateNameField, setShowStateNameField] = useState(false)
-  const [alertText, setAlertText] = useState('')
+    city: "",
+    lat: "",
+    lng: "",
+  });
+  const { city, lat, lng } = formData;
+  const [stateName, setStateName] = useState("");
+  const [country, setCountry] = useState("");
+  const [showCityField, setShowCityField] = useState(false);
+  const [showStateNameField, setShowStateNameField] = useState(false);
+  const [alertText, setAlertText] = useState("");
+  const [showSearchResults, setShowSearchResults] = useState(false);
 
   // for GSAP enter/exit animation
-  let ref = useRef(null)
+  let ref = useRef(null);
 
   // form control functions
   // -------------------------------------
-  const onSubmit = e => {
-    let query
-    e.preventDefault()
+  const onSubmit = (e) => {
+    let query = "";
+    e.preventDefault();
 
     if (!lat && !lng && !country) {
-      setAlertText('Please provide a country name or coordinates')
-    } else if(!countryList.includes(country)){
-      setAlertText('Please select a valid country')
+      setAlertText("Please provide a country name or coordinates");
+    } else if (!countryList.includes(country)) {
+      setAlertText("Please select a valid country");
     } else {
-      if (lat !== '' && lng !== '') {
+      if (lat !== "" && lng !== "") {
         // call sunrise/sunset api
         dispatch(fetchSunData({ lat, lng }))
-          .then(response => console.log(response))
-          .catch(error => console.log(error))
+          .then((response) => console.log(response))
+          .catch((error) => console.log(error));
       }
       // call APIs with form data
       else if (!(lat && lng)) {
-        query = `${city + ', ' || ''} ${stateName + ', ' || ''} ${country ||
-          ''} `
-        if (stateName && country !== 'United States') {
-          query += stateName
+        query += city ? city + ", " : "";
+        query += stateName ? stateName + ", " : "";
+        query += country ? country : "";
+        if (stateName && country !== "United States") {
+          query += stateName;
         }
         dispatch(fetchCoords(query))
-          .then(res => {
-            let results = res.payload.data.results
-            results = results.filter(result=>result.confidence <= 6)
-            console.log(results)
-
-            // call sunrise api
-          })
-          .catch(err => console.error(err))
+        setShowSearchResults(true)
       }
     }
-  }
+  };
 
-  const onChange = e => {
+  const onChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
   // ---------------------------------------
 
+  // show search results if they exist
+  // useEffect(()=>{
+  //   setShowSearchResults(coordsSearchResults.length > 0)
+  // }, [coordsSearchResults])
+
+  // clear state and city fields based on country value
   useEffect(() => {
-    if (country !== 'United States') {
-      setStateName('')
+    if (country !== "United States") {
+      setStateName("");
     } else if (!country) {
-      setFormData({ ...formData, city: '' })
+      setFormData({ ...formData, city: "" });
     }
-  }, [country, setStateName, setFormData])
+  }, [country, setStateName, setFormData]);
 
+  // enable/disable city/state fields based on country value
   useEffect(() => {
-    setShowCityField(country !== '')
-    setShowStateNameField(country === 'United States')
-  }, [country])
+    setShowCityField(country !== "");
+    setShowStateNameField(country === "United States");
+  }, [country]);
 
+  // form enter/exit animation
   useEffect(() => {
-    const tl = new TimelineLite()
+    const tl = new TimelineLite();
 
     // tween the form in or out based on the value of the showForm boolean
     let tween = TweenMax.to(ref, {
       y: showForm ? 0 : -500,
       scale: showForm ? 1 : 0,
       duration: 1,
-      ease: 'elastic.out(0.25, 0.25)'
-    })
+      ease: "elastic.out(0.25, 0.25)",
+    });
 
     // add the tween to the timeline
-    tl.add(tween)
+    tl.add(tween);
 
-    return () => tl.kill()
-  }, [showForm])
+    return () => tl.kill();
+  }, [showForm]);
 
   return (
     <form
-      id='form'
+      id="form"
       onSubmit={onSubmit}
-      ref={el => (ref = el)}
-      onKeyDown={e => {
+      ref={(el) => (ref = el)}
+      onKeyDown={(e) => {
         // override default action when Enter is pressed
         // to avoid erroneous form submissions when using Enter
         // to choose an option in the select menus
-        if (e.key === 'Enter') {
-          e.preventDefault()
+        if (e.key === "Enter") {
+          e.preventDefault();
         }
       }}
     >
-      {alertText !== '' && (
+      {alertText !== "" && (
         <Alert alertText={alertText} setAlertText={setAlertText} interval={3} />
       )}
 
-      <div id='header'>
-        <span className='close-x'>
+      {showSearchResults && (
+        <>
+        {/* <h1>{JSON.stringify(coordsSearchResults)}</h1> */}
+        <SearchResults results={coordsSearchResults} />
+        </>
+      )}
+
+      <div id="header">
+        <span className="close-x">
           {/* toggle hide/show form */}
           <GrClose onClick={() => dispatch(toggleForm())} />
         </span>
-        <span id='title'>Location</span>
+        <span id="title">Location</span>
       </div>
-      <div id='city-state-country'>
-        <div className='form-row'>
-          <p className='label'>
-            <span className='text'>Country</span>
+      <div id="city-state-country">
+        <div className="form-row">
+          <p className="label">
+            <span className="text">Country</span>
           </p>
           <AutoCompleteSelect
-            fieldName={'country'}
+            fieldName={"country"}
             value={country}
             allOptions={countryList}
             formDataSetter={setCountry}
@@ -150,62 +163,62 @@ const Form = () => {
           />
         </div>
 
-        <div className='form-row' id='state-field'>
-          <p className='label'>
-            <span className='text'>State</span>
+        <div className="form-row" id="state-field">
+          <p className="label">
+            <span className="text">State</span>
           </p>
-          {!showStateNameField ? <div className='disabled-form-row'></div> : ''}
+          {!showStateNameField ? <div className="disabled-form-row"></div> : ""}
           <AutoCompleteSelect
-            fieldName={'state'}
+            fieldName={"state"}
             value={stateName}
             allOptions={stateList}
             formDataSetter={setStateName}
             optionsShown={6}
-            isDisabled={country !== 'United States'}
+            isDisabled={country !== "United States"}
           />
         </div>
         <div
-          className={'form-row ' + (showCityField ? '' : 'hidden')}
-          id='city-field'
+          className={"form-row " + (showCityField ? "" : "hidden")}
+          id="city-field"
         >
-          <p className='label'>
-            <span className='text'>City</span>
+          <p className="label">
+            <span className="text">City</span>
           </p>
-          {!showCityField ? <div className='disabled-form-row'></div> : ''}
+          {!showCityField ? <div className="disabled-form-row"></div> : ""}
           <input
-            type='text'
-            name='city'
+            type="text"
+            name="city"
             value={city}
             onChange={onChange}
             disabled={!country}
           />
         </div>
       </div>
-      <div id='divider'>
-        <span id='circle'>
+      <div id="divider">
+        <span id="circle">
           <span>OR</span>
         </span>
       </div>
 
-      <div id='lat-lng'>
-        <div className='form-row'>
-          <p className='label'>
-            <span className='text'>Latitude</span>
+      <div id="lat-lng">
+        <div className="form-row">
+          <p className="label">
+            <span className="text">Latitude</span>
           </p>
-          <input type='text' name='lat' value={lat} onChange={onChange} />
+          <input type="text" name="lat" value={lat} onChange={onChange} />
         </div>
-        <div className='form-row'>
-          <p className='label'>Longitude</p>
-          <input type='text' name='lng' value={lng} onChange={onChange} />
+        <div className="form-row">
+          <p className="label">Longitude</p>
+          <input type="text" name="lng" value={lng} onChange={onChange} />
         </div>
 
-        <div className='form-row'>
-          <input type='submit' id='submit' value='Submit' />
+        <div className="form-row">
+          <input type="submit" id="submit" value="Submit" />
         </div>
       </div>
     </form>
     // </Transition>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- frontend/src/state/slices/sunClockSlice.js
+++ frontend/src/state/slices/sunClockSlice.js
@@ -1,97 +1,108 @@
 import {
   createSlice,
   createAsyncThunk,
-  rejectWithValue
-} from '@reduxjs/toolkit'
-import axios from 'axios'
-import { OPEN_CAGE_DATA_API_KEY } from '../../secrets'
+  rejectWithValue,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+import { OPEN_CAGE_DATA_API_KEY } from "../../secrets";
 
 export const fetchCoords = createAsyncThunk(
-  'sunClock/fetchCoords',
+  "sunClock/fetchCoords",
   // date: string - MM-DD-YYYY
   async (query, { rejectWithValue }) => {
-    const url = "https://api.opencagedata.com/geocode/v1/json"
-    try {
-      const response = await axios.get(url,{
+    const url = "https://api.opencagedata.com/geocode/v1/json";
+    const results = await axios
+      .get(url, {
         params: {
           query: query,
-          key: OPEN_CAGE_DATA_API_KEY
-        }
+          key: OPEN_CAGE_DATA_API_KEY,
+        },
       })
-      return response
-    } catch (error) {
-      return rejectWithValue(error)
-    }
+      .then((res) => res.data.results)
+      .catch((err) => rejectWithValue(err));
+    return results;
   }
-)
+);
 
 export const fetchSunData = createAsyncThunk(
-  'sunClock/fetchSunData',
+  "sunClock/fetchSunData",
   // date: string - MM-DD-YYYY
   async (coords, { rejectWithValue }) => {
-    const {lat, lng} = coords;
-    const url = 'https://api.sunrise-sunset.org/json'
+    const { lat, lng } = coords;
+    const url = "https://api.sunrise-sunset.org/json";
     try {
-      const response = await axios.get(url,{
+      const response = await axios.get(url, {
         params: {
           lat: lat,
-          lng: lng
-        }
-      })
-      return response
+          lng: lng,
+        },
+      });
+      return response.data;
     } catch (error) {
-      return rejectWithValue(error)
+      return rejectWithValue(error);
     }
   }
-)
+);
 
 const initialState = {
   lat: null,
   lng: null,
   sunriseTime: null,
   sunsetTime: null,
-  city: '',
-  state: '',
-  country: '',
-  showForm: true
-}
+  city: "",
+  state: "",
+  country: "",
+  showForm: true,
+  coordsSearchResults: [],
+};
 
 const sunClockSlice = createSlice({
-  name: 'sunClock',
+  name: "sunClock",
   initialState: initialState,
   reducers: {
     setSunData(state, action) {
       return {
         sunriseTime: action.payload.sunrise,
-        sunsetTime: action.payload.sunset
-      }
+        sunsetTime: action.payload.sunset,
+      };
     },
-    setCoords (state, action) {
+    setCoords(state, action) {
       return {
         lat: action.payload.lat,
-        lng: action.payload.lng
-      }
+        lng: action.payload.lng,
+      };
     },
-    setLocation (state, action) {
+    setLocation(state, action) {
       return {
         city: action.payload.city,
         state: action.payload.state,
-        country: action.payload.country
-      }
+        country: action.payload.country,
+      };
     },
-    toggleForm (state, action) {
+    toggleForm(state, action) {
       return {
-        showForm: !state.showForm
-      }
-    }
-  }
-})
+        showForm: !state.showForm,
+      };
+    },
+  },
+  extraReducers: {
+    // GET CONTACTS
+    [fetchCoords.pending]: (state, action) => {
+      state.coordsLoadingStatus = "PENDING";
+    },
+    [fetchCoords.rejected]: (state, action) => {
+      state.coordsSearchResults = [];
+      state.coordsLoadingStatus = "IDLE";
+    },
+    [fetchCoords.fulfilled]: (state, action) => {
+      console.log("payload", action);
+      state.coordsSearchResults = action.payload;
+      state.coordsLoadingStatus = "IDLE";
+    },
+  },
+});
 
-export const {
-  setSunTimes,
-  setCoords,
-  setLocation,
-  toggleForm
-} = sunClockSlice.actions
+export const { setSunTimes, setCoords, setLocation, toggleForm } =
+  sunClockSlice.actions;
 
-export default sunClockSlice.reducer
+export default sunClockSlice.reducer;
